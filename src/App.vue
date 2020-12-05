747056c5c0927a2dc052ab1d9c23d076a3317156
lgrdHenoc <lgrdHenoc@outlook.fr>
2020-12-05 08:56:52 +0100

    4. Computed Properties

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
+    <p>Spaces Left: {{ spacesLeft }} out of {{ capacity }}</p>
     <p>Capacity: {{ capacity }}</p>
     <button @click="increaseCapacity()">Increase Capacity</button>
+    <h2>Attending</h2>
+    <ul>
+      <li v-for='(name, index) in attending' :key='index'>
+        {{ name}}</li>
+    </ul>
   </div>
 
 </template>
 
 <script>
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
 export default {
   name: 'App',
   setup() {
-    const capacity = ref(3);
+    const capacity = ref(4);
+    const attending = ref(["Tim", "Bob", "Joe"]);
+
+    const spacesLeft = computed(()=> {
+      return capacity.value - attending.value.length;
+    })
 
     function increaseCapacity() {
       capacity.value++;
     }
-    return { capacity, increaseCapacity };
+    return { capacity, increaseCapacity, attending, spacesLeft };
   }
 }
 </script>
@@ -30,5 +41,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+li {
+  list-style-type: none;
 }
 </style>
