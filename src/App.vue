dc2a7786a5eb4fa88aa23f46605ccc69f45b23ca
lgrdHenoc <lgrdHenoc@outlook.fr>
2020-12-05 08:47:12 +0100

    2. Setup & Reactive References

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Capacity: {{ capacity }}</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import {ref} from "vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    const capacity = ref(3);
+
+    return { capacity };
   }
 }
 </script>
